8bca32ea9fee1ee1e670f04b8e8ec832a7b3ca3b
chongism <34437688+chongism@users.noreply.github.com>
2017-12-11 08:44:33 +0000

    Add files via upload

---
+++ BitLocker Status 0.3.ps1
@@ -0,0 +1,22 @@
+﻿$LogLocation = "C:\Windows\BitLocker Status Log.txt"
+
+Write-Output "***Bitlocker Encryption has started***" | Out-File $LogLocation
+
+while ($BitLocker -ne 100){
+
+    $BitLocker = (Get-BitLockerVolume | where -Property EncryptionPercentage).EncryptionPercentage
+    $ReportDate = Get-Date
+    $ReportDate = $ReportDate.ToString('dd/MM/yyyy hh:mm:ss') 
+    $Status = "Bitlocker Encryption Status $BitLocker%"
+
+    Start-Sleep -seconds 1
+
+    Write-Output $ReportDate
+
+    $ReportDate | Out-File $LogLocation
+    $Status | Out-File $LogLocation
+}
+
+Write-Output "BitLocker encryption completed at $Time" | Out-File $LogLocation
+
+[System.Windows.MessageBox]::Show('BitLocker Encryption Complete!')
---
+++ BuildVer 0.3.ps1
@@ -0,0 +1,63 @@
+﻿########### INSERT BUILD VERSION TO REGISTRY ############
+#
+# Author: Max Chong
+# Company: ASOS
+# Date: 08/12/17
+# Version: 0.3 
+#
+#########################################################
+
+
+# Define COM object
+
+$TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+
+# Retrieve Package Name (TS Build name)
+
+$TSName = $TSEnv.Value("_SMSTSPackageName")
+$WIMImage = $TSEnv.Value("OSDImageName")
+$WinOS = (Get-WmiObject -class Win32_OperatingSystem).Caption
+$OSVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId
+$BuiltOn = get-date -Format "dd/MM/yy"
+
+# Set Registry Entry
+
+Push-Location
+Set-Location HKLM:
+
+# Check if path key already exists. If location does not exist, create entry and write to the registry key.
+# Else, output 
+
+$PathCheck = Test-Path .\SYSTEM\TSVer
+
+if ($PathCheck -eq $false)
+    {
+ 
+        New-Item -Path .\SYSTEM -Name TSVer
+        New-ItemProperty -Path .\SYSTEM\TSVer -Name "TSName" -PropertyType "String" -Value $TSName
+        New-ItemProperty -Path .\SYSTEM\TSVer -Name "WIMImage" -PropertyType "String" -Value $WIMImage
+        New-ItemProperty -Path .\SYSTEM\TSVer -Name "WinOS" -PropertyType "String" -Value $WinOS
+        New-ItemProperty -Path .\SYSTEM\TSVer -Name "OSVersion" -PropertyType "String" -Value $OSVersion
+        New-ItemProperty -Path .\SYSTEM\TSVer -Name "BuiltOn" -PropertyType "String" -Value $BuiltOn
+        Pop-Location
+
+        cls
+
+        ######## Test Output #######
+        
+        <#
+        echo "Build version entered successfully!"
+        echo "Task Sequence Name: $TSName"
+        echo "WIM Image: $WIMImage"
+        echo "Windows Version: $WinOS"
+        echo "Version: $OSVersion"
+        echo "Built on: $BuiltOn"
+        #>
+
+    } else {
+        cls
+        echo "Entry already exists!"
+    }
+
+
+######################## END ##########################
